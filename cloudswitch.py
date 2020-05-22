4abee37ba36b0a0e3e341e99355f0b36b20c5faa
Jordi Prats <jordi.prats@gmail.com>
2020-05-22 11:46:30 +0200

    typo

--- cloudswitch.py
+++ cloudswitch.py
@@ -5,7 +5,7 @@ import boto3
 import getopt
 
 def eprint(*args, **kwargs):
-    #print(*args, file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 def showJelp(msg):
     print("Usage: "+sys.argv[0])
